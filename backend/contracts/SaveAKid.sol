ace68035eead75f08d1c93f574a3a893b5822c9f
kihiuFrank <franklinekihiu@gmail.com>
2023-10-19 07:28:32 +0300

    donate func

--- backend/contracts/SaveAKid.sol
+++ backend/contracts/SaveAKid.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.7;
 
+// Errors
+error InputsCantBeNull();
+error DeadlineShouldBeInFuture();
+error AmountDonatedMustBeGreaterThanZero(uint minAmount, uint donatedAmount);
+error DeadlineReached(uint campaignDeadline, uint timeRequested);
+
 contract SaveAKid {
     struct Home {
         address owner;
@@ -10,6 +16,27 @@ contract SaveAKid {
         string image;
         uint256 contactPerson;
     }
+
+    struct Campaign {
+        address owner;
+        string name;
+        string category;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        uint256[] donations;
+    }
+
+    event Action(
+        uint256 id,
+        string actionType,
+        address indexed executor,
+        uint256 timestamp
+    );
 
     mapping(uint256 => Home) public homes;
     mapping(address => uint) balances;
@@ -48,5 +75,70 @@ contract SaveAKid {
 
         numberOfHomes++;
         return numberOfHomes - 1;
+    }
+
+    function createCampaign(
+        address _owner,
+        string memory _name,
+        string memory _title,
+        string memory _category,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadline,
+        string memory _image
+    ) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        // check that the dealine is in the future
+        require(
+            campaign.deadline < block.timestamp,
+            "deadline should be a date in the future"
+        );
+
+        campaign.owner = _owner;
+        campaign.name = _name;
+        campaign.title = _title;
+        campaign.category = _category;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+    }
+
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        // amount donated shouldn't be zero or less
+        if (amount <= 0 wei) {
+            revert AmountDonatedMustBeGreaterThanZero({
+                minAmount: 1 wei,
+                donatedAmount: amount
+            });
+        }
+
+        // cannot donate after deadline
+        if (campaign.deadline < block.timestamp) {
+            revert DeadlineReached({
+                campaignDeadline: campaigns[_id].deadline,
+                timeRequested: block.timestamp
+            });
+        }
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent, ) = payable(manager).call{value: amount}("");
+        require(sent, "donation failed");
+
+        if (sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
     }
 }
